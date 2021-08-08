1bbf3cf3a1a94bc218c9a0b21d02164f4220e85c
Wes Madaj <wtmadaj@gmail.com>
2021-08-08 19:05:23 -0400

    Initial commit. Structure for CV API

---
+++ src/test/java/com/wtmadaj/cvapi/CvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtmadaj.cvapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
